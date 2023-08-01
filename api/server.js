b25b09811d1635d303ab7261addefd5a3672755d
babotavo419 <one.faith419@gmail.com>
2023-07-31 20:30:41 -0400

    ok

--- api/server.js
+++ api/server.js
@@ -1,3 +1,78 @@
-// BUILD YOUR SERVER HERE
+const express = require('express');
+const {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+  resetDB, // ONLY TESTS USE THIS ONE
+} = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json()); // this allows parsing of JSON request bodies
+
+// set up endpoints
+server.get('/api/users', async (req, res) => {
+  try {
+    const users = await find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get users' });
+  }
+});
+
+server.get('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get user' });
+  }
+});
+
+server.post('/api/users', async (req, res) => {
+  const newUser = req.body;
+  try {
+    const user = await insert(newUser);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to create new user' });
+  }
+});
+
+server.put('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+    const user = await update(id, changes);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to update user' });
+  }
+});
+
+server.delete('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await remove(id);
+    if (deleted) {
+      res.json(deleted);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete user' });
+  }
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const server = require('./api/server');
 
 const port = 9000;
 
-// START YOUR SERVER HERE
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+

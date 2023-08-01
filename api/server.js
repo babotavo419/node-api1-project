1e4cdd13d9f5ec8d324bf76d7476e586b33b4acf
babotavo419 <one.faith419@gmail.com>
2023-07-31 20:42:16 -0400

    ok

--- api/server.js
+++ api/server.js
@@ -23,56 +23,65 @@ server.get('/api/users', async (req, res) => {
 });
 
 server.get('/api/users/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await findById(id);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'User not found' });
+    const { id } = req.params;
+    try {
+      const user = await findById(id);
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: 'User does not exist.' });
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to get user' });
     }
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to get user' });
-  }
-});
+  });  
 
 server.post('/api/users', async (req, res) => {
-  const newUser = req.body;
-  try {
-    const user = await insert(newUser);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to create new user' });
-  }
-});
-
-server.put('/api/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const changes = req.body;
-  try {
-    const user = await update(id, changes);
-    if (user) {
-      res.json(user);
+    const newUser = req.body;
+    if (!newUser.name || !newUser.bio) {
+      res.status(400).json({ message: 'Please provide name and bio for the user.' });
     } else {
-      res.status(404).json({ message: 'User not found' });
+      try {
+        const user = await insert(newUser);
+        res.status(201).json(user);
+      } catch (err) {
+        res.status(500).json({ message: 'Failed to create new user' });
+      }
     }
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to update user' });
-  }
-});
+  });
+  
+  server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    if (!changes.name || !changes.bio) {
+      res.status(400).json({ message: 'Please provide name and bio for the user.' });
+    } else {
+      try {
+        const user = await update(id, changes);
+        if (user) {
+          res.json(user);
+        } else {
+          res.status(404).json({ message: 'User does not exist.' });
+        }
+      } catch (err) {
+        res.status(500).json({ message: 'Failed to update user' });
+      }
+    }
+  });  
 
 server.delete('/api/users/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deleted = await remove(id);
-    if (deleted) {
-      res.json(deleted);
-    } else {
-      res.status(404).json({ message: 'User not found' });
+    const { id } = req.params;
+    try {
+      const deleted = await remove(id);
+      if (deleted) {
+        res.json(deleted);
+      } else {
+        res.status(404).json({ message: 'User does not exist.' });
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to delete user' });
     }
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to delete user' });
-  }
-});
+  });
+  
 
 module.exports = server;
